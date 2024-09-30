8a78d2c1fe4fb940129bea85447c0b432c9bd8fe
Semen Saifutdinov <saifutdinov2006@mail.ru>
2024-09-30 20:23:55 +0300

    Add docstring to inline

--- keyboards/inline/inline_keyboards.py
+++ keyboards/inline/inline_keyboards.py
@@ -2,6 +2,11 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 def get_inline_subscribe(product_id):
+    """
+    Функция для рендеринга inline кнопок
+    :param product_id: ID продукта
+    :return: набор кнопок
+    """
     builder = InlineKeyboardBuilder()
     builder.button(
         text="Подписаться", callback_data=f"subscribe_{product_id}"
