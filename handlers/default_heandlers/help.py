a5e3c1603180d826657d002fa85adc7952e6386d
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-02-08 12:01:40 +0300

    Added loging to default handlers

--- handlers/default_heandlers/help.py
+++ handlers/default_heandlers/help.py
@@ -3,6 +3,7 @@ from config_data.config import DEFAULT_COMMANDS
 from aiogram import F, Router
 from aiogram.filters import Command
 
+from loader import app_logger
 
 router = Router()
 
@@ -10,4 +11,5 @@ router = Router()
 @router.message(F.text, Command("help"))
 async def bot_help(message: Message):
     text = [f'/{command[1]} - {desk[1]}' for command, desk in DEFAULT_COMMANDS]
+    app_logger.info(f"Пользователь {message.from_user.full_name} вызвал команду /help")
     await message.answer('Доступные команды:\n{}'.format("\n".join(text)))
--- handlers/default_heandlers/start.py
+++ handlers/default_heandlers/start.py
@@ -2,13 +2,14 @@ from aiogram.types import Message
 from aiogram import Router
 from aiogram.filters import CommandStart
 from keyboards.reply.reply_keyboards import get_menu
-
+from loader import app_logger
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def bot_start(message: Message):
+    app_logger.info(f"Пользователь {message.from_user.full_name} запустил бота!")
     await message.answer(f"""
     Здравствуйте, {message.from_user.full_name}! Я - телеграм бот.
 Чтобы узнать все мои команды, введите /help
