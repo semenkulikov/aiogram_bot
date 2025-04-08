ebbfe50c1bf02b198510943b4d00652753850895
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-04-08 23:58:15 +0300

    Added docs to handlers

--- handlers/custom_heandlers/handlers.py
+++ handlers/custom_heandlers/handlers.py
@@ -41,7 +41,7 @@ async def get_info_product(message: Message, state: FSMContext, session: AsyncSe
         await message.answer("Введите корректный артикул!")
         return
 
-    # URL для получения информации о карточках товаров.
+
     path = f"https://card.wb.ru/cards/v1/detail?appType=1&curr=rub&dest=-1257786&spp=30&nm={article}"
     async with ClientSession() as http_session:
         async with http_session.get(path) as response:
